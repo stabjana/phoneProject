4b989c065f6de62be168f5d71735075bcf0aca69
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-02-10 12:33:47 +0200

    add get type function

--- phoneRegister.js
+++ phoneRegister.js
@@ -1,16 +1,28 @@
-'use strict';
+"use strict";
 
 module.exports = class PhoneRegister {
-    #register
+    #register;
 
     constructor(data) {
         if (!data) {
-            throw new Error('phone data missing');
+            throw new Error("phone data missing");
         }
         this.#register = data;
     }
 
     getTypes() {
+        // array that only has one type once: mapping and set, or for loop
+        const foundTypes = [];
+        for (const person of this.#register) {
+            for (const phone of person.phones) {
+                if (!foundTypes.includes(phone.type)) {
+                    foundTypes.push(phone.type);
+                }
+            }
+        }
 
-    }
-}
+        return foundTypes;
+    } // end of get types
+
+
+};
